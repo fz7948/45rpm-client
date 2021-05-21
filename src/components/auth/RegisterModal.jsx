c5f3094f98f141df552a8348edeb402499b02098
O Wooyoung <fz7948@gmail.com>
2021-05-21 12:44:24 +0900

    feat:유효성 검사 및 코드 최적화

--- src/components/auth/RegisterModal.jsx
+++ src/components/auth/RegisterModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { ModalBack, ModalBox } from '../common/ModalStyle';
 import styled from 'styled-components';
 
@@ -20,6 +20,15 @@ const RegisterWrapper = styled.div`
     position: relative;
     list-style: none;
     margin-bottom: 1rem;
+  }
+  .deny-message {
+    width: 220px;
+    height: 12px;
+    font-size: 12px;
+    font-weight: 500;
+    margin-bottom: 1.2rem;
+    color: #f73d5c;
+    word-break: keep-all;
   }
 `;
 
@@ -95,6 +104,140 @@ const RegisterModal = ({ open, close }) => {
   const [denyMessage, setDenyMessage] = useState('');
 
   useEffect(() => {
+    refEmail.current.focus();
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        handleCloseBtn();
+      }
+    });
+  }, [open]);
+
+  const handleMoveToUsername = (e) => {
+    if (e.key === 'Enter') {
+      refUsername.current.focus();
+    }
+  };
+
+  const handleMoveTopassword = (e) => {
+    if (e.key === 'Enter') {
+      refPassword.current.focus();
+    }
+  };
+
+  const handleMoveTopasswordCheck = (e) => {
+    if (e.key === 'Enter') {
+      refPasswordCheck.current.focus();
+    }
+  };
+
+  const handleMoveToSignUp = (e) => {
+    if (e.key === 'Enter') {
+      handleSignup();
+    }
+  };
+
+  const handleChangeEmail = useCallback(
+    (e) => {
+      setInputEmail(e.target.value);
+    },
+    [inputEmail],
+  );
+
+  const handleChangeUsername = useCallback(
+    (e) => {
+      setInputUsername(e.target.value);
+    },
+    [inputUsername],
+  );
+
+  const handleChangePassword = useCallback(
+    (e) => {
+      setInputPassword(e.target.value);
+    },
+    [inputPassword],
+  );
+
+  const handleChangePasswordCheck = useCallback(
+    (e) => {
+      setInputPasswordCheck(e.target.value);
+    },
+    [inputPasswordCheck],
+  );
+
+  const handleCloseBtn = () => {
+    setInputEmail('');
+    setInputUsername('');
+    setInputPassword('');
+    setInputPasswordCheck('');
+    setDenyMessage('');
+    close();
+  };
+
+  const checkValidEmail = useCallback(
+    (email) => {
+      if (
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          email,
+        )
+      ) {
+        return true;
+      }
+      setDenyMessage('유효하지 않은 이메일입니다.');
+      return false;
+    },
+    [inputEmail, denyMessage],
+  );
+
+  const checkValidPassword = useCallback(
+    (password) => {
+      if (!/^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,20}$/.test(password)) {
+        setDenyMessage(
+          '영문자 + 숫자/특수문자 조합으로 8~20자리를 사용해야 합니다.',
+        );
+        return false;
+      }
+      const check_num = password.search(/[0-9]/g);
+      const check_eng = password.search(/[a-z]/gi);
+      if (check_num < 0 || check_eng < 0) {
+        setDenyMessage('비밀번호는 숫자와 영문자를 혼용하여야 합니다.');
+        return false;
+      }
+      if (/(\w)\1\1\1/.test(password)) {
+        setDenyMessage('비밀번호에 같은 문자를 4번 이상 사용하실 수 없습니다.');
+        return false;
+      }
+      return true;
+    },
+    [inputPassword, denyMessage],
+  );
+
+  const handleCheckForm = () => {
+    if (inputEmail === '') {
+      refEmail.current.focus();
+      setDenyMessage('E-mail을 입력하세요');
+      return false;
+    } else if (!checkValidEmail(inputEmail)) {
+      refEmail.current.focus();
+      return false;
+    }
+    if (inputUsername === '') {
+      refUsername.current.focus();
+      setDenyMessage('닉네임을 입력하세요');
+      return false;
+    }
+    if (!checkValidPassword(inputPassword)) {
+      refPassword.current.focus();
+      return false;
+    }
+    if (inputPassword !== inputPasswordCheck) {
+      refPasswordCheck.current.focus();
+      setDenyMessage('비밀번호와 일치하지 않습니다');
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
     if (localVisible && !open) {
       setAnimate(true);
       setTimeout(() => setAnimate(false), 250);
@@ -105,7 +248,10 @@ const RegisterModal = ({ open, close }) => {
   if (!animate && !localVisible) return null;
 
   const handleSignup = () => {
-    //api
+    // 여기서 api 요청 보내야함 !!
+    if (handleCheckForm()) {
+      alert('요청을 성공적으로 보냈습니다');
+    }
   };
 
   return (
@@ -123,10 +269,11 @@ const RegisterModal = ({ open, close }) => {
                   <div>E-mail</div>
                 </RegisterLabel>
                 <RegisterInput
-                  type="email"
-                  value={email || ''}
-                  onChange={registerOnChange}
+                  type="text"
+                  value={inputEmail}
+                  onChange={handleChangeEmail}
                   placeholder="사용하실 E-mail을 입력해주세요."
+                  onKeyPress={handleMoveToUsername}
                   ref={refEmail}
                 />
               </li>
@@ -136,9 +283,10 @@ const RegisterModal = ({ open, close }) => {
                 </RegisterLabel>
                 <RegisterInput
                   type="text"
-                  value={username || ''}
-                  onChange={registerOnChange}
+                  value={inputUsername}
+                  onChange={handleChangeUsername}
                   placeholder="사용하실 닉네임을 입력해주세요."
+                  onKeyPress={handleMoveTopassword}
                   ref={refUsername}
                 />
               </li>
@@ -148,9 +296,10 @@ const RegisterModal = ({ open, close }) => {
                 </RegisterLabel>
                 <RegisterInput
                   type="password"
-                  value={password || ''}
-                  onChange={registerOnChange}
+                  value={inputPassword}
+                  onChange={handleChangePassword}
                   placeholder="Password"
+                  onKeyPress={handleMoveTopasswordCheck}
                   ref={refPassword}
                 />
               </li>
@@ -160,13 +309,15 @@ const RegisterModal = ({ open, close }) => {
                 </RegisterLabel>
                 <RegisterInput
                   type="password"
-                  value={PW_confirm || ''}
-                  onChange={registerOnChange}
+                  value={inputPasswordCheck}
+                  onChange={handleChangePasswordCheck}
                   placeholder="Password Check"
+                  onKeyPress={handleMoveToSignUp}
                   ref={refPasswordCheck}
                 />
               </li>
             </ul>
+            <p className="deny-message">{denyMessage}</p>
             <RegisterSubmitBtn onClick={handleSignup}>
               회원가입
             </RegisterSubmitBtn>
