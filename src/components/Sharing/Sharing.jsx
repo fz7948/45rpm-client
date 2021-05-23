359e6240e2bce547cec4f6aac5c9d8d8f71a06cf
DLSKAGHD <dlskaghd234@gmail.com>
2021-05-24 00:14:47 +0900

    ModifiedDesign Sharing Page

--- src/components/Sharing/Sharing.jsx
+++ src/components/Sharing/Sharing.jsx
@@ -3,43 +3,59 @@ import Header from '../common/Header';
 import Sidebar from '../common/Sidebar';
 import { useHistory } from 'react-router-dom';
 import { SharingArr } from '../data/SharingData';
-import Footer from '../common/Footer';
+
 import {
   Container,
   ContentWrapper,
-  ImgWrapper,
-  LeftWrapper,
-  Img,
-  SharingWrapper,
+  TitleWrapper,
+  H1Title,
+  SubTitle,
   ContinueBtn,
-  ImgArray,
-  Image,
+  Button,
+  CdCaseContent,
+  CoverImg,
+  Img,
+  Disk,
+  InnerDisk,
+  Img1,
 } from '../common/SharingStyle';
 
 const Sharing = () => {
   const history = useHistory();
+  const [color, setColor] = useState('');
+
+  const handleChangeColor = () => {
+    setColor();
+  };
 
   return (
     <Container>
       <Header />
+      <TitleWrapper>
+        <H1Title>세상에 하나밖에 없는 당신만의 LP</H1Title>
+        <SubTitle>
+          3억 명의 회원이 45RPM의 커스텀 LP를 소유하고 있습니다
+        </SubTitle>
+      </TitleWrapper>
       <ContentWrapper>
-        <ImgWrapper>
-          <Img src="./images/1.jpg" />
-        </ImgWrapper>
-        <LeftWrapper>
-          <SharingWrapper>
-            <ImgArray>
-              {/**Axios request */}
-              {SharingArr.map((el) => (
-                <Image src={el.img} alt="" key={el.id} />
-              ))}
-            </ImgArray>
-          </SharingWrapper>
-          <ContinueBtn onClick={() => history.push('/1')}>
-            계속 만들기
-          </ContinueBtn>
-        </LeftWrapper>
+        {SharingArr.map((el) => {
+          return (
+            <CdCaseContent key={el.id}>
+              <CoverImg>
+                <Img src={el.coverImage} />
+              </CoverImg>
+              <Disk>
+                <InnerDisk>
+                  <Img1 src={el.innerImage} />
+                </InnerDisk>
+              </Disk>
+            </CdCaseContent>
+          );
+        })}
       </ContentWrapper>
+      <ContinueBtn>
+        <Button>계속 만들기 </Button>
+      </ContinueBtn>
     </Container>
   );
 };
