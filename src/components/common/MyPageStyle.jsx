59f1e8df283715652205a2cb3063cab220d05453
DLSKAGHD <dlskaghd234@gmail.com>
2021-05-24 01:30:48 +0900

    MyPage Revised

--- src/components/common/MyPageStyle.jsx
+++ src/components/common/MyPageStyle.jsx
@@ -1,54 +1,43 @@
 import styled from 'styled-components';
 
-export const MypageWrapper = styled.div `
+export const MyPageWrapper = styled.div `
   display: flex;
   flex-direction: column;
   padding-top: 10px;
   width: 100vw;
   height: 100vh;
-  background: linear-gradient(120deg, #03154E, #311788);
-  }
-  p {
-    margin-bottom: 0.8rem;
-  }
-  .side {
-    position: sticky;
-    left: 90rem;
-    height: 2rem;
-    width: 2rem;
-    opacity: 0.6;
-    cursor: pointer;
-  }
-  .infoBtn {
-     width:100%;
-     display:flex;
-     justify-content:center;
-
-     @media screen and (max-width:768px) {
-        padding-right:3.2rem;
-     }
-  }
+  background: linear-gradient(120deg, #03154e, #311788);
 `;
 
-export const MypageContent = styled.div `
+export const MyPageContent = styled.div `
   display: flex;
+  flex-direction: column;
+  flex: 1;
   justify-content: space-around;
   align-items: center;
-  width: 50%;
-  height: 83vh;
-  padding: 2.5rem 1.6rem;
+  width: 60%;
+  height: 100%;
   margin: 0 auto;
-  background: #e8e8e8;
+  margin-top: 4rem;
+  padding: 1rem;
+  border: 1px solid #fff;
   border-radius: 6px;
-  margin-top: 10px;
-
   @media screen and (max-width: 768px) {
-    width: 70%;
+    width: 90%;
+    height: 100%;
   }
 `;
 
-export const MypageImage = styled.div `
-  flex: 1.2;
+export const MyPageInfoWrapper = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const MyPageImage = styled.div `
+  flex: 1;
   cursor: pointer;
   display: flex;
   flex-direction: column;
@@ -57,9 +46,10 @@ export const MypageImage = styled.div `
   border-radius: 6px;
   height: 200px;
   border: 2px dashed #ccc;
+  color: #fff;
   img {
     height: 1rem;
-    opacity: 0.6;
+    opacity: 0.5;
   }
   p {
     opacity: 0.7;
@@ -72,47 +62,63 @@ export const MypageImage = styled.div `
   }
 `;
 
-export const MypageInfo = styled.div `
-  flex: 2;
+export const MyPageInfo = styled.div `
+  flex: 1.5;
   height: 200px;
   margin-left: 1rem;
   background: #ccc;
   padding: 30px;
   font-size: small;
+  @media screen and (max-width: 768px) {
+    flex: 1;
+  }
+  p {
+    padding: 0.5rem;
+    font-size: 1.3rem;
+    font-family: 'Lato', sans-serif;
+    @media screen and (max-width: 768px) {
+      padding: 0.5rem;
+      font-size: 1rem;
+    }
+  }
 `;
 
-export const MypageSlide = styled.div `
+export const MyPageSlide = styled.div `
+  flex: 1.5;
   display: flex;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
-  width: 70%;
+  width: 100%;
+  height: 100%;
   margin-top: 3rem;
 
   @media screen and (max-width: 768px) {
     display: flex;
     margin: 0 auto;
     margin-top: 2rem;
-    width: 90%;
+    width: 100%;
+    height: 100%;
   }
 `;
 
-export const MypageButton = styled.button `
+export const MyPageButton = styled.button `
+  border: 3px solid black;
   cursor: pointer;
-  margin-top: 1rem;
   background-color: #eee;
-  height: 3.5rem;
-  width: 9rem;
+  height: 5rem;
+  width: 13rem;
   border-radius: 3px;
-  border: 0;
   outline: 0;
+  font-family: 'Jua', sans-serif;
+  font-size: 1.5rem;
   &:hover {
     background-color: #ddd;
     transition: all ease 0.4s;
   }
-
   @media screen and (max-width: 768px) {
     height: 2.5rem;
     width: 8.5rem;
+    font-size: 1rem;
   }
 `;
