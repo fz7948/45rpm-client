2eba0e28e8a3ba2e7f58ab1729c581920fb1c410
tyufjvbn2 <tyufjvbn22@gamil.com>
2021-05-27 04:48:49 +0900

    fix:카카오 소셜 로그아웃시 오류 수정

--- src/components/auth/LoginModal.jsx
+++ src/components/auth/LoginModal.jsx
@@ -254,6 +254,9 @@ const LoginModal = ({ open, close, history }) => {
                 dispatch(kakaoLoginReq(response)).then(() => {
                   alert('기본 비밀번호는 카카오 계정의 이메일 주소입니다');
                   history.push('/mypage');
+                  dispatch(resetLogin());
+                  dispatch(resetLoginMsg());
+                  close();
                 });
               },
               fail: function (error) {
