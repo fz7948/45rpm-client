50c7d5aa9a393a93e2662efa4a644934cc496f69
O Wooyoung <fz7948@gmail.com>
2021-06-03 01:40:26 +0900

    docs:일부 수정

--- src/components/common/SharingStyle.jsx
+++ src/components/common/SharingStyle.jsx
@@ -1,7 +1,7 @@
 import { PictureAsPdf, SmsFailed } from '@material-ui/icons';
 import styled from 'styled-components';
 
-export const Container = styled.div `
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
@@ -20,7 +20,7 @@ export const Container = styled.div `
   }
 `;
 
-export const TitleWrapper = styled.div `
+export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   padding: 4rem;
@@ -32,7 +32,7 @@ export const TitleWrapper = styled.div `
   }
 `;
 
-export const H1Title = styled.h1 `
+export const H1Title = styled.h1`
   color: #ffffff;
   font-size: 4.7rem;
   font-family: 'Lato', sans-serif;
@@ -43,7 +43,7 @@ export const H1Title = styled.h1 `
   }
 `;
 
-export const SubTitle = styled.h5 `
+export const SubTitle = styled.h5`
   color: #ffffff;
   font-size: 2rem;
 
@@ -52,7 +52,7 @@ export const SubTitle = styled.h5 `
   }
 `;
 
-export const ContentWrapper = styled.div `
+export const ContentWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-row-gap: 6rem;
@@ -81,14 +81,14 @@ export const ContentWrapper = styled.div `
   }
 `;
 
-export const CdCaseContent = styled.div `
+export const CdCaseContent = styled.div`
   display: flex;
   width: 70%;
   height: 100%;
   position: relative;
 `;
 
-export const CoverImg = styled.div `
+export const CoverImg = styled.div`
   flex: 1;
   width: 270px;
   height: 270px;
@@ -103,7 +103,7 @@ export const CoverImg = styled.div `
   }
 `;
 
-export const Img = styled.img `
+export const Img = styled.img`
   width: inherit;
   height: inherit;
   position: absolute;
@@ -112,7 +112,7 @@ export const Img = styled.img `
   border-radius: 3px;
 `;
 
-export const Disk = styled.div `
+export const Disk = styled.div`
   flex: 1;
   width: 250px;
   height: 250px;
@@ -151,7 +151,7 @@ export const Disk = styled.div `
   }
 `;
 
-export const InnerDisk = styled.div `
+export const InnerDisk = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
@@ -181,7 +181,7 @@ export const InnerDisk = styled.div `
   }
 `;
 
-export const Img1 = styled.img `
+export const Img1 = styled.img`
   width: 90px;
   height: 90px;
   border-radius: 50%;
@@ -196,7 +196,7 @@ export const Img1 = styled.img `
   }
 `;
 
-export const ContinueBtn = styled.div `
+export const ContinueBtn = styled.div`
   width: 100%;
   height: 10vh;
   display: flex;
@@ -217,7 +217,7 @@ export const ContinueBtn = styled.div `
   }
 `;
 
-export const Button = styled.button `
+export const Button = styled.button`
   cursor: pointer;
   width: 10rem;
   height: 3rem;
