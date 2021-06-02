b60e9f8aff3758bdab7026a07cf5badf876447d5
DLSKAGHD <dlskaghd234@gmail.com>
2021-06-02 21:40:36 +0900

    Revised SharingPage

--- src/components/common/SharingStyle.jsx
+++ src/components/common/SharingStyle.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div `
   display: flex;
   flex-direction: column;
   width: 100vw;
@@ -19,7 +19,7 @@ export const Container = styled.div`
   }
 `;
 
-export const TitleWrapper = styled.div`
+export const TitleWrapper = styled.div `
   display: flex;
   flex-direction: column;
   padding: 4rem;
@@ -31,7 +31,7 @@ export const TitleWrapper = styled.div`
   }
 `;
 
-export const H1Title = styled.h1`
+export const H1Title = styled.h1 `
   color: #ffffff;
   font-size: 4.7rem;
   font-family: 'Lato', sans-serif;
@@ -42,7 +42,7 @@ export const H1Title = styled.h1`
   }
 `;
 
-export const SubTitle = styled.h5`
+export const SubTitle = styled.h5 `
   color: #ffffff;
   font-size: 2rem;
 
@@ -51,7 +51,7 @@ export const SubTitle = styled.h5`
   }
 `;
 
-export const ContentWrapper = styled.div`
+export const ContentWrapper = styled.div `
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-row-gap: 6rem;
@@ -80,14 +80,14 @@ export const ContentWrapper = styled.div`
   }
 `;
 
-export const CdCaseContent = styled.div`
+export const CdCaseContent = styled.div `
   display: flex;
   width: 70%;
   height: 100%;
   position: relative;
 `;
 
-export const CoverImg = styled.div`
+export const CoverImg = styled.div `
   flex: 1;
   width: 270px;
   height: 270px;
@@ -102,7 +102,7 @@ export const CoverImg = styled.div`
   }
 `;
 
-export const Img = styled.img`
+export const Img = styled.img `
   width: inherit;
   height: inherit;
   position: absolute;
@@ -111,10 +111,10 @@ export const Img = styled.img`
   border-radius: 3px;
 `;
 
-export const Disk = styled.div`
+export const Disk = styled.div `
   flex: 1;
-  width: 280px;
-  height: 280px;
+  width: 250px;
+  height: 250px;
   position: absolute;
   top: 6%;
   left: 35%;
@@ -150,7 +150,7 @@ export const Disk = styled.div`
   }
 `;
 
-export const InnerDisk = styled.div`
+export const InnerDisk = styled.div `
   position: absolute;
   top: 50%;
   left: 50%;
@@ -195,7 +195,7 @@ export const Img1 = styled.img `
   }
 `;
 
-export const ContinueBtn = styled.div`
+export const ContinueBtn = styled.div `
   width: 100%;
   height: 10vh;
   display: flex;
@@ -216,7 +216,7 @@ export const ContinueBtn = styled.div`
   }
 `;
 
-export const Button = styled.button`
+export const Button = styled.button `
   cursor: pointer;
   width: 10rem;
   height: 3rem;
