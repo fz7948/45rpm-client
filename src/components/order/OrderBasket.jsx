c307a6d4b949f26fb9a84a7d7abc2532e5965fc3
DLSKAGHD <dlskaghd234@gmail.com>
2021-06-05 17:48:41 +0900

    Test

--- src/components/order/OrderBasket.jsx
+++ src/components/order/OrderBasket.jsx
@@ -138,24 +138,25 @@ const OrderBasket = ({ cartItems, onAdd, onRemove }) => {
   return (
     <>
       <BasketWrapper>
-        <BasketTitle> 결제 목록 </BasketTitle>
+        <BasketTitle>결제 목록</BasketTitle>
         <BasketOrderList>
           <BasketOrder>
             <div>
               {cartItems.length === 0 && (
-                <div className="small"> 결제 대기중인 내역이 없습니다. </div>
+                <div className="small">결제 대기중인 내역이 없습니다.</div>
               )}
             </div>
           </BasketOrder>
           {cartItems.map((item) => (
             <BasketOrder key={item._id}>
-              <textarea> {item.title} </textarea>
+              <textarea>{item.title}</textarea>
               <div>
-                <button onClick={() => onAdd(item)}> + </button> {item.qty}
-                <button onClick={() => onRemove(item)}> - </button>
+                <button onClick={() => onAdd(item)}>+</button>
+                {item.qty}
+                <button onClick={() => onRemove(item)}>-</button>
               </div>
               <div className="small">
-                {10000 + item.songList.length * 2000} 원
+                {10000 + item.songList.length * 2000}원
               </div>
             </BasketOrder>
           ))}
@@ -164,22 +165,24 @@ const OrderBasket = ({ cartItems, onAdd, onRemove }) => {
           {cartItems.length !== 0 && (
             <>
               <div className="price">
-                <div> 상품 가격 </div> <div> {itemsPrice}원 </div>
+                <div>상품 가격</div>
+                <div>{itemsPrice}원</div>
               </div>
               <div className="price">
-                <div> 부가세 </div> <div> {taxPrice.toFixed(0)}원 </div>
+                <div>부가세</div>
+                <div>{taxPrice.toFixed(0)}원</div>
               </div>
               <div className="price">
-                <div> 총 합계 </div>
+                <div>총 합계</div>
                 <div>
-                  <strong> {totalPrice.toFixed(0)}원 </strong>
+                  <strong>{totalPrice.toFixed(0)}원</strong>
                 </div>
               </div>
             </>
-          )}{' '}
-          <PriceBtn onClick={payHandler}> 결제하기 </PriceBtn>{' '}
-        </BasketPrice>{' '}
-      </BasketWrapper>{' '}
+          )}
+          <PriceBtn onClick={payHandler}>결제하기</PriceBtn>
+        </BasketPrice>
+      </BasketWrapper>
     </>
   );
 };
